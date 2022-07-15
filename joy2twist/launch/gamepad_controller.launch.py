3054681f1da8dfbf51b7771985ba50fa99a496b4
pawelirh <pawel.irzyk@husarion.com>
2022-07-15 14:08:46 +0200

    Fixed launch file

--- joy2twist/launch/gamepad_controller.launch.py
+++ joy2twist/launch/gamepad_controller.launch.py
@@ -1,20 +1,31 @@
+import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    ld.add_action(ExecuteProcess(
-        cmd=[["ros2 run joy_linux joy_linux_node"]],
-        shell=True
-    ))
+    joy2twist_cfg_path = os.path.join(get_package_share_directory(
+        'joy2twist'), 'config', 'joy2twist.yaml')
+
+    declare_joy2twist_params_file_argument = DeclareLaunchArgument(
+        'joy2twist_params_file',
+        default_value=joy2twist_cfg_path,
+        description='ROS2 parameters file to use with joy2twist node')
+    ld.add_action(declare_joy2twist_params_file_argument)
+
+    run_joy_linux = ExecuteProcess(
+        cmd=[['ros2 run joy_linux joy_linux_node']],
+        shell=True)
+    ld.add_action(run_joy_linux)
 
     launch_joy2twist = create_include_launch(package='joy2twist',
                                              rel_launch_path='/launch/joy2twist.launch.py',
-                                             arguments={})
+                                             arguments={'joy2twist_params_file': LaunchConfiguration('joy2twist_params_file')})
     ld.add_action(launch_joy2twist)
 
     return ld
