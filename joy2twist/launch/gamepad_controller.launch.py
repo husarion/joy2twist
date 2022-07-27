3a8f98008b837d181c25ef84bfa093f3c5119856
pawelirh <pawel.irzyk@husarion.com>
2022-07-27 11:34:19 +0200

    Join path corrected in launch file

--- joy2twist/launch/gamepad_controller.launch.py
+++ joy2twist/launch/gamepad_controller.launch.py
@@ -1,13 +1,10 @@
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    IncludeLaunchDescription,
-)
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
@@ -24,7 +21,7 @@ def generate_launch_description():
 
     joy2twist_launch = create_include_launch(
         package="joy2twist",
-        rel_launch_path="/launch/joy2twist.launch.py",
+        rel_launch_path="launch/joy2twist.launch.py",
         arguments={
             "joy2twist_params_file": LaunchConfiguration("joy2twist_params_file")
         },
@@ -43,9 +40,10 @@ def generate_launch_description():
 
 
 def create_include_launch(package: str, rel_launch_path: str, arguments: dict):
-    share_dir_path = get_package_share_directory(package)
     included_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(share_dir_path + rel_launch_path),
+        PythonLaunchDescriptionSource(
+            [PathJoinSubstitution([FindPackageShare(package), rel_launch_path])]
+        ),
         launch_arguments=arguments.items(),
     )
     return included_launch
--- joy2twist/launch/joy2twist.launch.py
+++ joy2twist/launch/joy2twist.launch.py
@@ -1,8 +1,9 @@
-from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
