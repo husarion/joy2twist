80bb4c5c423c0df4deeeeb40ef44aa9695a6b474
Dawid <kmakd197@gmail.com>
2023-12-01 12:17:48 +0000

    add e-stop handle

--- joy2twist/include/joy2twist/joy2twist_node.hpp
+++ joy2twist/include/joy2twist/joy2twist_node.hpp
@@ -9,11 +9,15 @@
 
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/joy.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_srvs/srv/trigger.hpp>
 
 namespace joy2twist
 {
 using MsgJoy = sensor_msgs::msg::Joy;
 using MsgTwist = geometry_msgs::msg::Twist;
+using MsgBool = std_msgs::msg::Bool;
+using SrvTrigger = std_srvs::srv::Trigger;
 
 struct ButtonIndex
 {
@@ -24,6 +28,10 @@ struct ButtonIndex
   int dead_man_switch;
   int fast_mode;
   int slow_mode;
+
+  int e_stop_reset;
+  int e_stop_trigger;
+  int enable_e_stop_reset;
 };
 
 class Joy2TwistNode : public rclcpp::Node
@@ -35,18 +43,30 @@ private:
   void declare_parameters();
   void load_parameters();
 
+  void e_stop_cb(const std::shared_ptr<MsgBool> bool_msg);
   void joy_cb(const std::shared_ptr<MsgJoy> joy_msg);
   void convert_joy_to_twist(const std::shared_ptr<MsgJoy> joy_msg, MsgTwist & twist_msg);
   std::pair<float, float> determine_velocity_factor(const std::shared_ptr<MsgJoy> joy_msg);
+  void call_trigger_service(const rclcpp::Client<SrvTrigger>::SharedPtr & client);
+  void trigger_service_cb(
+    const rclcpp::Client<SrvTrigger>::SharedFuture & future, const std::string & service_name);
 
   std::map<std::string, float> linear_velocity_factors_;
   std::map<std::string, float> angular_velocity_factors_;
 
   ButtonIndex button_index_;
   bool driving_mode_;
+  bool e_stop_present_;
+  bool e_stop_state_;
+  std::string e_stop_topic_;
+  std::string e_stop_reset_srv_;
+  std::string e_stop_trigger_srv_;
 
+  rclcpp::Subscription<MsgBool>::SharedPtr e_stop_sub_;
   rclcpp::Subscription<MsgJoy>::SharedPtr joy_sub_;
   rclcpp::Publisher<MsgTwist>::SharedPtr twist_pub_;
+  rclcpp::Client<SrvTrigger>::SharedPtr e_stop_reset_client_;
+  rclcpp::Client<SrvTrigger>::SharedPtr e_stop_trigger_client_;
 };
 
 static constexpr char FAST[]{"fast"};
--- joy2twist/src/joy2twist_node.cpp
+++ joy2twist/src/joy2twist_node.cpp
@@ -2,6 +2,8 @@
 
 namespace joy2twist
 {
+using std::placeholders::_1;
+
 Joy2TwistNode::Joy2TwistNode() : Node("joy2twist_node")
 {
   using namespace std::placeholders;
@@ -9,8 +11,17 @@ Joy2TwistNode::Joy2TwistNode() : Node("joy2twist_node")
   declare_parameters();
   load_parameters();
 
-  joy_sub_ = create_subscription<MsgJoy>("joy", rclcpp::SensorDataQoS(), std::bind(&Joy2TwistNode::joy_cb, this, _1));
-  twist_pub_ = create_publisher<MsgTwist>("cmd_vel", rclcpp::QoS(rclcpp::KeepLast(1)).durability_volatile().reliable());
+  joy_sub_ = create_subscription<MsgJoy>(
+    "joy", rclcpp::SensorDataQoS(), std::bind(&Joy2TwistNode::joy_cb, this, _1));
+  twist_pub_ = create_publisher<MsgTwist>(
+    "cmd_vel", rclcpp::QoS(rclcpp::KeepLast(1)).durability_volatile().reliable());
+
+  if (e_stop_present_) {
+    e_stop_sub_ = this->create_subscription<MsgBool>(
+      e_stop_topic_, rclcpp::SystemDefaultsQoS(), std::bind(&Joy2TwistNode::e_stop_cb, this, _1));
+    e_stop_reset_client_ = this->create_client<SrvTrigger>(e_stop_reset_srv_);
+    e_stop_trigger_client_ = this->create_client<SrvTrigger>(e_stop_trigger_srv_);
+  }
 
   RCLCPP_INFO(get_logger(), "Initialized node!");
 }
@@ -23,12 +34,22 @@ void Joy2TwistNode::declare_parameters()
   this->declare_parameter<float>("angular_velocity_factor.fast", 1.0);
   this->declare_parameter<float>("angular_velocity_factor.regular", 0.5);
   this->declare_parameter<float>("angular_velocity_factor.slow", 0.2);
-  this->declare_parameter<int>("button_index_map.axis.angular_z", 0);
-  this->declare_parameter<int>("button_index_map.axis.linear_x", 3);
-  this->declare_parameter<int>("button_index_map.axis.linear_y", 2);
+
+  this->declare_parameter<bool>("e_stop.present", false);
+  this->declare_parameter<std::string>("e_stop.topic", "e_stop");
+  this->declare_parameter<std::string>("e_stop.reset_srv", "e_stop_reset");
+  this->declare_parameter<std::string>("e_stop.trigger_srv", "e_stop_trigger");
+
+  // why axis mapping has different default values than for ros1
+  this->declare_parameter<int>("button_index_map.axis.angular_z", 2);
+  this->declare_parameter<int>("button_index_map.axis.linear_x", 1);
+  this->declare_parameter<int>("button_index_map.axis.linear_y", 0);
   this->declare_parameter<int>("button_index_map.dead_man_switch", 4);
   this->declare_parameter<int>("button_index_map.fast_mode", 7);
   this->declare_parameter<int>("button_index_map.slow_mode", 5);
+  this->declare_parameter<int>("button_index_map.e_stop_reset", 1);
+  this->declare_parameter<int>("button_index_map.e_stop_trigger", 2);
+  this->declare_parameter<int>("button_index_map.enable_e_stop_reset", 6);
 }
 
 void Joy2TwistNode::load_parameters()
@@ -39,17 +60,41 @@ void Joy2TwistNode::load_parameters()
   this->get_parameter<float>("angular_velocity_factor.fast", angular_velocity_factors_[FAST]);
   this->get_parameter<float>("angular_velocity_factor.regular", angular_velocity_factors_[REGULAR]);
   this->get_parameter<float>("angular_velocity_factor.slow", angular_velocity_factors_[SLOW]);
+
+  this->get_parameter<bool>("e_stop.present", e_stop_present_);
+  this->get_parameter<std::string>("e_stop.topic", e_stop_topic_);
+  this->get_parameter<std::string>("e_stop.reset_srv", e_stop_reset_srv_);
+  this->get_parameter<std::string>("e_stop.trigger_srv", e_stop_trigger_srv_);
+
   this->get_parameter<int>("button_index_map.axis.angular_z", button_index_.angular_z);
   this->get_parameter<int>("button_index_map.axis.linear_x", button_index_.linear_x);
   this->get_parameter<int>("button_index_map.axis.linear_y", button_index_.linear_y);
   this->get_parameter<int>("button_index_map.dead_man_switch", button_index_.dead_man_switch);
   this->get_parameter<int>("button_index_map.fast_mode", button_index_.fast_mode);
   this->get_parameter<int>("button_index_map.slow_mode", button_index_.slow_mode);
+  this->get_parameter<int>("button_index_map.e_stop_reset", button_index_.e_stop_reset);
+  this->get_parameter<int>("button_index_map.e_stop_trigger", button_index_.e_stop_trigger);
+  this->get_parameter<int>(
+    "button_index_map.enable_e_stop_reset", button_index_.enable_e_stop_reset);
 }
+
+void Joy2TwistNode::e_stop_cb(const MsgBool::SharedPtr bool_msg) { e_stop_state_ = bool_msg->data; }
 
 void Joy2TwistNode::joy_cb(const MsgJoy::SharedPtr joy_msg)
 {
   MsgTwist twist_msg;
+
+  if (e_stop_present_) {
+    if (joy_msg->buttons.at(button_index_.e_stop_trigger) && !e_stop_state_) {
+      twist_pub_->publish(twist_msg);
+      call_trigger_service(e_stop_trigger_client_);
+    } else if (
+      joy_msg->buttons.at(button_index_.enable_e_stop_reset) &&
+      joy_msg->buttons.at(button_index_.e_stop_reset) && e_stop_state_) {
+      call_trigger_service(e_stop_reset_client_);
+    }
+  }
+
   if (joy_msg->buttons.at(button_index_.dead_man_switch)) {
     driving_mode_ = true;
     convert_joy_to_twist(joy_msg, twist_msg);
@@ -84,6 +129,33 @@ std::pair<float, float> Joy2TwistNode::determine_velocity_factor(const MsgJoy::S
     angular_velocity_factor = angular_velocity_factors_.at(FAST);
   }
   return std::make_pair(linear_velocity_factor, angular_velocity_factor);
+}
+
+void Joy2TwistNode::call_trigger_service(const rclcpp::Client<SrvTrigger>::SharedPtr & client)
+{
+  if (!client->wait_for_service(std::chrono::milliseconds(2000))) {
+    RCLCPP_ERROR(this->get_logger(), "Can't contact %s service", client->get_service_name());
+    return;
+  }
+
+  auto request = std::make_shared<SrvTrigger::Request>();
+
+  client->async_send_request(request, [&](const rclcpp::Client<SrvTrigger>::SharedFuture future) {
+    trigger_service_cb(future, client->get_service_name());
+  });
+}
+
+void Joy2TwistNode::trigger_service_cb(
+  const rclcpp::Client<SrvTrigger>::SharedFuture & future, const std::string & service_name)
+{
+  if (!future.get()->success) {
+    RCLCPP_ERROR(
+      this->get_logger(), "Failed to call %s service: %s", service_name.c_str(),
+      future.get()->message.c_str());
+    return;
+  }
+
+  RCLCPP_INFO(this->get_logger(), "Successfully called %s service", service_name.c_str());
 }
 
 }  // namespace joy2twist
